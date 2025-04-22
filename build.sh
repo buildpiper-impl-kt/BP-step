5e447d6a58e9fa0f71f4a33e2dfc9973da76521d
Aayush Verma <aayush.verma@mygurukulam.co>
2025-04-22 15:03:39 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function archive_name() {
+     local path_of_file=$1
+
+     if [ -z "$path_of_file" ]; then
+        echo "Path is empty. Please provide a valid path." 
+
+     else 
+        if [ -e "$path_of_file" ]; then
+            archive_name=$(basename "$path_of_file")_$(date +%Y%m%d%H%M%S).tar.gz
+            tar -czvf "/data/$archive_name" "$path_of_file"
+            echo "File successfully zipped as: $archive_name"
+        else
+            echo "Error: Path does not exist."
+        fi
+    fi
+}
+
+archive_name "$1"
