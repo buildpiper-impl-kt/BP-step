2ac22a8221cb1414f1d546d8bf05d90d55428d6b
Aayush Verma <aayush.verma@mygurukulam.co>
2025-05-06 17:20:40 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,16 @@ source /opt/buildpiper/shell-functions/functions.sh
 source /opt/buildpiper/shell-functions/log-functions.sh
 
 function increase_size() {
-    local asg_name=$1
-    local asg_max_size=$2
-    local region=$3
+    local cluster_name=$1
+    local node_group_name=$2
+    local min_size=$3
+    local max_size=$4
+    local desired_size=$5
+    local region=$6
 
-    if [[ -z "$asg_name" || -z "$asg_max_size" || -z "$region" ]]; then
+    if [[ -z "$cluster_name" || -z "$node_group_name" || -z "$min_size" || -z "$max_size" || -z "$desired_size" || -z "$region" ]]; then
         echo "Error: Missing arguments."
-        echo "Usage: $0 asg name, asg size, region."
+        echo "Usage: $0 cluster_name, node group name, min size, max size, desired size, region."
         exit 1
     fi
 
@@ -18,7 +21,7 @@ function increase_size() {
         exit 2
     fi
 
-    output=$(aws autoscaling update-auto-scaling-group --auto-scaling-group-name "$asg_name" --max-size "$asg_max_size" --region "$region" 2>&1)
+    output=$(aws eks update-nodegroup-config --cluster-name "$cluster_name" --nodegroup-name "$node_group_name" --scaling-config minSize="$min_size",maxSize="$max_size",desiredSize="$desired_size" --region "$region" 2>&1)
     status=$?
 
     if [[ $status -ne 0 ]]; then
@@ -26,10 +29,8 @@ function increase_size() {
         echo "Output: $output"
         exit $status
     else
-        echo "Successfully updated auto scaling group $asg_name with max size $asg_max_size in region $region."
+        echo "Successfully updated node group '$node_group_name' in cluster '$cluster_name' with max size $max_size , min size $min_size , and desired size $desired_size in region $region."         
     fi
 }
 
-increase_size "$1" "$2" "$3"
-
-
+increase_size "$1" "$2" "$3" "$4" "$5" "$6"
