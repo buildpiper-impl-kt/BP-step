36306a2fae278b8c7afa73ffe13c177af4147944
Aayush Verma <aayush.verma@mygurukulam.co>
2025-05-06 16:33:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ source /opt/buildpiper/shell-functions/log-functions.sh
 
 function increase_size() {
     local asg_name=$1
-    local asg_size=$2
+    local asg_max_size=$2
     local region=$3
 
-    if [[ -z "$asg_name" || -z "$asg_size" || -z "$region" ]]; then
+    if [[ -z "$asg_name" || -z "$asg_max_size" || -z "$region" ]]; then
         echo "Error: Missing arguments."
         echo "Usage: $0 asg name, asg size, region."
         exit 1
@@ -18,7 +18,7 @@ function increase_size() {
         exit 2
     fi
 
-    output=$(aws autoscaling update-auto-scaling-group --auto-scaling-group-name "$asg_name" --max-size "$asg_size" --region "$region" 2>&1)
+    output=$(aws autoscaling update-auto-scaling-group --auto-scaling-group-name "$asg_name" --max-size "$asg_max_size" --region "$region" 2>&1)
     status=$?
 
     if [[ $status -ne 0 ]]; then
@@ -26,9 +26,10 @@ function increase_size() {
         echo "Output: $output"
         exit $status
     else
-        echo "Successfully updated auto scaling group $asg_name with max size $asg_size in region $region."
+        echo "Successfully updated auto scaling group $asg_name with max size $asg_max_size in region $region."
     fi
 }
 
 increase_size "$1" "$2" "$3"
+
 
