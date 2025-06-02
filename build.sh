2af779a9883d1c572a17def5527bace02b4ee900
Aayush Verma <aayush.verma@mygurukulam.co>
2025-06-02 12:25:17 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -57,52 +57,13 @@ local source_code="$1"
   fi
     new_dep_name="$dep_dir/${source_dir}-dependencies_$timestamp.xlsx"
     mv ./"$source_dir"-dependencies.xlsx "$new_dep_name"
-
-  else
-    if [ -d "$source_code" ]; then
-      echo "Using local directory: $source_code"
-      cd "$source_code"
-
-      /usr/bin/porting-advisor "$source_code" --output "$(basename "$source_code")".html
-      /usr/bin/porting-advisor  "$source_code" --output "$(basename "$source_code")"-dependencies.xlsx --output-format dependencies
-      timestamp=$(date +%Y%m%d%H%M%S)
-   base_dir=~/graviton_report
-
-  if [ ! -d "$base_dir" ]; then
-    mkdir -p "$base_dir"
-    echo "Created base directory: $base_dir"
-  fi
-
-  report_dir="$base_dir/report"
-  if [ ! -d "$report_dir" ]; then
-    mkdir -p "$report_dir"
-    echo "Created directory $report_dir"
-  else
-    echo "Directory $report_dir already exists"
-  fi
-
-    new_name="$report_dir/$(basename "$source_code")_$timestamp.html"
-    mv "$(basename "$source_code")".html "$new_name"
-
-
-  dep_dir="$base_dir/dependencies"
-  if [ ! -d "$dep_dir" ]; then
-    mkdir -p "$dep_dir"
-    echo "Created directory $dep_dir"
-  else
-    echo "Directory $dep_dir already exists"
-  fi
-    new_dep_name="$dep_dir/$(basename "$source_code")-dependencies_$timestamp.xlsx"
-    mv "$(basename "$source_code")"-dependencies.xlsx "$new_dep_name"
-    else
-      echo "Directory '$source_code' does not exist."
-      return 1
-    fi
   fi
 }
 
-graviton "$1"
+
+
+graviton "$SOURCE_CODE"
 
 
 
-# sudo docker run -it -v ~/graviton_report:/root/graviton_report check https://github.com/OT-MICROSERVICES/attendance-api.git
+# sudo docker run -it   -v ~/graviton_report:/root/graviton_report   -e SOURCE_CODE=https://github.com/OT-MICROSERVICES/attendance-api.git   <image_name>
