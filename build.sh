876d104f233330b8f25ee3fe9c510d1d8d62b473
Aayush Verma <aayush.verma@mygurukulam.co>
2025-06-02 11:27:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,35 +2,107 @@
 source /opt/buildpiper/shell-functions/functions.sh
 source /opt/buildpiper/shell-functions/log-functions.sh
 
-function increase_size() {
-    local cluster_name=$1
-    local node_group_name=$2
-    local min_size=$3
-    local max_size=$4
-    local desired_size=$5
-    local region=$6
+clone() {
+local source_code="$1"
 
-    if [[ -z "$cluster_name" || -z "$node_group_name" || -z "$min_size" || -z "$max_size" || -z "$desired_size" || -z "$region" ]]; then
-        echo "Error: Missing arguments."
-        echo "Usage: $0 cluster_name, node group name, min size, max size, desired size, region."
-        exit 1
-    fi
+  if [ -z "$source_code" ]; then
+    echo "Git URL or local directory path is required"
+    return 1
+  fi
 
-    if ! command -v aws &> /dev/null; then
-        echo "Error: AWS CLI is not installed."
-        exit 2
-    fi
+  if [[ "$source_code" == *.git ]]; then
+    local source_dir
+    source_dir=$(basename -s .git "$source_code")
 
-    output=$(aws eks update-nodegroup-config --cluster-name "$cluster_name" --nodegroup-name "$node_group_name" --scaling-config minSize="$min_size",maxSize="$max_size",desiredSize="$desired_size" --region "$region" 2>&1)
-    status=$?
+    mkdir -p ~/source_repo
+    echo "Using directory ~/source_repo"
+    cd ~/source_repo || return 1
 
-    if [[ $status -ne 0 ]]; then
-        echo "Error: Failed to update Auto Scaling group."
-        echo "Output: $output"
-        exit $status
+    if [ -d "$source_dir" ]; then
+      echo "Directory '$source_dir' already exists."
     else
-        echo "Successfully updated node group '$node_group_name' in cluster '$cluster_name' with max size $max_size , min size $min_size , and desired size $desired_size in region $region."         
+      echo "Cloning $source_code..."
+      git clone "$source_code"
     fi
+    cd
+    /usr/bin/porting-advisor ~/source_repo/"$source_dir" --output "$source_dir".html
+    /usr/bin/porting-advisor  ~/source_repo/"$source_dir" --output "$source_dir"-dependencies.xlsx --output-format dependencies
+    
+   timestamp=$(date +%Y%m%d%H%M%S)
+   base_dir=~/graviton_report
+
+  if [ ! -d "$base_dir" ]; then
+    mkdir -p "$base_dir"
+    echo "Created base directory: $base_dir"
+  fi
+
+  report_dir="$base_dir/report"
+  if [ ! -d "$report_dir" ]; then
+    mkdir -p "$report_dir"
+    echo "Created directory $report_dir"
+  else
+    echo "Directory $report_dir already exists"
+  fi
+
+    new_name="$report_dir/${source_dir}_$timestamp.html"
+    mv ./"$source_dir".html "$new_name"
+
+
+  dep_dir="$base_dir/dependencies"
+  if [ ! -d "$dep_dir" ]; then
+    mkdir -p "$dep_dir"
+    echo "Created directory $dep_dir"
+  else
+    echo "Directory $dep_dir already exists"
+  fi
+    new_dep_name="$dep_dir/${source_dir}-dependencies_$timestamp.xlsx"
+    mv ./"$source_dir"-dependencies.xlsx "$new_dep_name"
+
+  else
+    if [ -d "$source_code" ]; then
+      echo "Using local directory: $source_code"
+      cd "$source_code"
+
+      /usr/bin/porting-advisor "$source_code" --output "$(basename "$source_code")".html
+      /usr/bin/porting-advisor  "$source_code" --output "$(basename "$source_code")"-dependencies.xlsx --output-format dependencies
+      timestamp=$(date +%Y%m%d%H%M%S)
+   base_dir=~/graviton_report
+
+  if [ ! -d "$base_dir" ]; then
+    mkdir -p "$base_dir"
+    echo "Created base directory: $base_dir"
+  fi
+
+  report_dir="$base_dir/report"
+  if [ ! -d "$report_dir" ]; then
+    mkdir -p "$report_dir"
+    echo "Created directory $report_dir"
+  else
+    echo "Directory $report_dir already exists"
+  fi
+
+    new_name="$report_dir/$(basename "$source_code")_$timestamp.html"
+    mv "$(basename "$source_code")".html "$new_name"
+
+
+  dep_dir="$base_dir/dependencies"
+  if [ ! -d "$dep_dir" ]; then
+    mkdir -p "$dep_dir"
+    echo "Created directory $dep_dir"
+  else
+    echo "Directory $dep_dir already exists"
+  fi
+    new_dep_name="$dep_dir/$(basename "$source_code")-dependencies_$timestamp.xlsx"
+    mv "$(basename "$source_code")"-dependencies.xlsx "$new_dep_name"
+    else
+      echo "Directory '$source_code' does not exist."
+      return 1
+    fi
+  fi
 }
 
-increase_size "$1" "$2" "$3" "$4" "$5" "$6"
+clone "$1"
+
+
+
+# sudo docker run -it -v ~/graviton_report:/root/graviton_report check https://github.com/OT-MICROSERVICES/attendance-api.git
