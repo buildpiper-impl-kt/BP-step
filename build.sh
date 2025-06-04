9b1745d76f5fa0ab090ef1c492ac4d952d9cd4b7
Aayush Verma <aayush.verma@mygurukulam.co>
2025-06-04 17:34:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,26 +7,21 @@ CODEBASE_LOCATION="${WORKSPACE}"/"${CODEBASE_DIR}"
 
 graviton_scan() {
 
-  cd ${CODEBASE_LOCATION}
-
   if [ -z "$CODEBASE_LOCATION" ]; then
     echo "Local directory path is required"
     return 1
   fi
-
-  source_dir=$(basename "$CODEBASE_LOCATION")
-    /usr/bin/porting-advisor "$CODEBASE_LOCATION" --output "$source_dir".html
-    /usr/bin/porting-advisor  "$CODEBASE_LOCATION" --output "$source_dir"-dependencies.xlsx --output-format dependencies
+    /usr/bin/porting-advisor "$CODEBASE_LOCATION" --output "$CODEBASE_DIR".html
+    /usr/bin/porting-advisor  "$CODEBASE_LOCATION" --output "$CODEBASE_DIR"-dependencies.xlsx --output-format dependencies
     
-   timestamp=$(date +%Y%m%d%H%M%S)
-   base_dir=~/graviton_report
+   base_dir="$CODEBASE_LOCATION/graviton_report" #chnage
 
   if [ ! -d "$base_dir" ]; then
     mkdir -p "$base_dir"
     echo "Created base directory: $base_dir"
   fi
 
-  report_dir="$base_dir/report"
+  report_dir="$base_dir/html_report"
   if [ ! -d "$report_dir" ]; then
     mkdir -p "$report_dir"
     echo "Created directory $report_dir"
@@ -34,8 +29,7 @@ graviton_scan() {
     echo "Directory $report_dir already exists"
   fi
 
-    new_name="$report_dir/${source_dir}_$timestamp.html"
-    mv ./"$source_dir".html "$new_name"
+    mv ./"$CODEBASE_DIR".html "$report_dir"
 
 
   dep_dir="$base_dir/dependencies"
@@ -45,8 +39,7 @@ graviton_scan() {
   else
     echo "Directory $dep_dir already exists"
   fi
-    new_dep_name="$dep_dir/${source_dir}-dependencies_$timestamp.xlsx"
-    mv ./"$source_dir"-dependencies.xlsx "$new_dep_name"
+    mv ./"$CODEBASE_DIR"-dependencies.xlsx "$dep_dir"
 }
 
 if [ -d "${CODEBASE_LOCATION}" ]; then
@@ -56,6 +49,8 @@ else
   exit 1
 fi
 
-
-
-
+#cp -rf reports/* /bp/execution_dir/${GLOBAL_TASK_ID}/
+#dest_dir="/bp/execution_dir/${GLOBAL_TASK_ID}/"
+dest_dir="/bp/execution_dir/test/"
+mkdir -p "$dest_dir"
+cp -rf "$base_dir"/* "$dest_dir"
